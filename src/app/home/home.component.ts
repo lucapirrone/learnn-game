3ed231daea30d471cf0768e3f2e276c41491893f
Luca Pirrone <lpirrone2000@gmail.com>
2021-07-05 11:48:55 +0200

    username personalizzato
    aggiunta possibilita di inserire il proprio username

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,12 +15,14 @@ export class HomeComponent implements OnInit {
   ModalitaEnum = Modalita;
   modalita: Modalita | null = null;
   fastGame: boolean = false;
+  username: string;
   round: number | null = null;
 
   constructor(private router: Router, private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       this.modalita = params.modalita ? params.modalita : null;
       this.round = params.round ? params.round : null;
+      this.username = params.username ? params.username : null;
       this.fastGame = params.fastGame === "true";
     });
   }
@@ -29,14 +31,22 @@ export class HomeComponent implements OnInit {
 
   setModalita(modalita: Modalita) {
     this.modalita = modalita;
-    this.router.navigate([], {queryParams: {round: this.round, fastGame: this.fastGame, modalita}});
+    this.router.navigate([], {queryParams: {round: this.round, fastGame: this.fastGame, modalita: this.modalita, username: this.username}});
+  }
+  setUsername(username: string) {
+    if (username.length > 8) {
+      alert("L'username non deve superare 8 caratteri");
+      return;
+    }
+    this.username = username;
+    this.router.navigate([], {queryParams: {round: this.round, fastGame: this.fastGame, modalita: this.modalita, username: this.username}});
   }
   setRound(round: number) {
     this.round = round;
     //this.router.navigate([], {queryParams: {modalita: this.modalita, round}});
 
     if (this.modalita && this.round) {
-      this.router.navigate(['game'], {queryParams: {modalita: this.modalita, round: this.round, fastGame: this.fastGame}});
+      this.router.navigate(['game'], {queryParams: {round: this.round, fastGame: this.fastGame, modalita: this.modalita, username: this.username}});
     }
   }
 
